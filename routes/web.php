69bf4791eb7e24166416f1ba61846052c91ae0ce
kenneth <kenneth.c@agentsofvalue.com>
2023-07-26 13:12:32 +0800

    Route using Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelloWorldController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('hw.hello');
-});
+// Route::get('/', function () {
+//     return view('hw.hello');
+// });
+
+Route::get('/', HelloWorldController::class);
+
+
