db946b8069c515086fd1ac2131f5de0ab5d866da
kenneth <kenneth.c@agentsofvalue.com>
2023-07-26 10:22:12 +0800

    First commit

---
+++ resources/views/hw/hello.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body class="bg-cyan-400 flex">
+     <div class="bg-violet-500 border-4 border-indigo-800 w-fit h-fit rounded-lg m-auto flex drop-shadow-2xl">
+        <h1 class="m-auto font-extrabold text-6xl p-8">
+            Hello World!
+        </h1>
+    </div> 
+</body>
+</html>
